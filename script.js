eaba84ec60f09b66296233619d4357ad6c1b0fd9
FranciscoJavierSuarez <fjsr1098@gmail.com>
2022-08-29 00:08:42 -0500

    AvanceProyectoFinal

--- script.js
+++ script.js
@@ -1,16 +1,17 @@
-$(document).ready(function(){
-    $(window).scroll(function(){
-        if(this.scrollY > 20){
+$(document).ready(function () {
+    $(window).scroll(function () {
+        if (this.scrollY > 20) {
             $('.navbar').addClass("sticky");
-        }else{
+        } else {
             $('.navbar').removeClass("sticky");
         }
     });
-    
-   // toggle menu/navbar script
-   
-   $('.menu-btn').click(function(){
-    $('.navbar .menu').toggleClass("active");
-    $('.menu-btn i').toggleClass("active");
-   });
+
+    // toggle menu/navbar script
+    let movil = "fas fa-bars", cerra = "fa-solid fa-xmark";
+    $('.menu-btn').html(`<i class="${movil}"></i>`);
+    $('.menu-btn').click(function () {
+        $('.navbar .menu').toggleClass("active");
+        $('.menu-btn').html(`<i class="${($(".navbar .menu").hasClass("active")) ? cerra : movil}"></i>`);
+    });
 });
